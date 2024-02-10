583f8553e7b69205ec33d971370ab7b327f9f2a0
Matt Goldman <matt.goldman@outlook.com>
2024-02-10 20:59:13 +1100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿namespace OrientationTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        private double _screenWidth;
+        public double ScreenWidth
+        {
+            get => _screenWidth;
+            set
+            {
+                _screenWidth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _boxViewWidth;
+        public double BoxViewWidth
+        {
+            get => _boxViewWidth;
+            set
+            {
+                _boxViewWidth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void StateTrigger_IsActiveChanged(object sender, EventArgs e)
+        {
+            var trigger = (StateTriggerBase)sender;
+
+            if (trigger.IsActive)
+            {
+                OrientationLabel.Text = "Orientation: Portrait";
+            }
+            else
+            {
+                OrientationLabel.Text = "Orientation: Landscape";
+            }
+
+            Console.WriteLine($"[OrientationState] Portrait: {trigger.IsActive}");
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            ScreenWidth = width;
+            BoxViewWidth = width - 400;
+        }
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OrientationTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
